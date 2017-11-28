32ce67d601ed93d6a30a8ff3f42f8133e66ff52a
Matthew Macdonald-Wallace <matt@doics.co>
2017-11-28 17:04:59 +0000

    added instructions for standalone air quality sensor

--- thingspeak_update.py
+++ thingspeak_update.py
@@ -16,64 +16,90 @@
 import sys, httplib, urllib, time, Adafruit_BMP.BMP085, DHT22, pigpio, atexit
 BMP085sensor=Adafruit_BMP.BMP085.BMP085()
 from config import *
+from air_quality import sensor as airqsense
 
 # Setup RPi GPIO pins
 PIN_DHT22=8
-PIN_DHT22i=7
 PIN_BMP085_SDA=0
 PIN_BMP085_SCL=0
 PIN_TGS2600=0
+PIN_AIRQ=7
 
 
 # update thingspeak routine (max once every 15 seconds)
 def update_thingspeak(data1, data2, data3, data4):
-	params = urllib.urlencode({"field1": data1, "field2": data2, "field3": data3, "field4": data4,'key':THINGSPEAK_APIWRITE})
-	headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
-	conn = httplib.HTTPConnection("api.thingspeak.com:80")                
-	try:
-		conn.request("POST", "/update", params, headers)
-		#response = conn.getresponse()
-		#print response.status, response.reason
-		#data = response.read()
-		conn.close()
-	except:
-		pass
-		# error handler if needed
+        params = urllib.urlencode({"field1": data1, "field2": data2, "field3": data3, "field4": data4,'key':THINGSPEAK_APIWRITE})
+        headers = {"Content-typZZe": "application/x-www-form-urlencoded","Accept": "text/plain"}
+        conn = httplib.HTTPConnection("api.thingspeak.com:80")                
+        try:
+                conn.request("POST", "/update", params, headers)
+                #response = conn.getresponse()
+                #print response.status, response.reason
+                #data = response.read()
+                conn.close()
+        except:
+                pass
+                # error handler if needed
 
 
 # main loop
 if __name__ == "__main__":
 
-	pi = pigpio.pi()
+     while True:
+     
+        s = airqsense(pi, PIN_AIRQ) # set the GPIO pin number
+  
+        # Use 30s for a properly calibrated reading.
+        time.sleep(30) 
+        
+        # get the gpio, ratio and concentration in particles / 0.01 ft3
+        g, r, c = s.read()
+  
+        if (c==1114000.62):
+            print("Error\n")
+            continue
+  
+        print( "Air Quality Measurements for PM2.5:" )
+        print( "  " + str(int(c)) + " particles/0.01ft^3" )
+  
+        # convert to SI units
+        concentration_ugm3=s.pcs_to_ugm3(c)
+        print( "  " + str(int(concentration_ugm3)) + " ugm^3")
+        
+        # convert SI units to US AQI
+        # input should be 24 hour average of ugm3, not instantaneous reading
+        aqi=s.ugm3_to_aqi(concentration_ugm3)
+        
+        print( "  Current AQI (not 24 hour avg): " + str(int(aqi)))
+        print( "" )
+  
+  
+        # Humidity and temp from DHT22 (outisde)
+        s = DHT22.sensor(pi, PIN_DHT22, LED=None, power=8)   
+        s.trigger()
+        time.sleep(0.2)
+        humidity=s.humidity()
+        temp1=s.temperature()
+        
+#       si = DHT22.sensor(pi, PIN_DHT22i, LED=None, power=8)   
+#       si.trigger()
+#       time.sleep(0.2)
+#       humidityi=si.humidity()
+        
+        # temp and pressure from BMP085
+        temp2=BMP085sensor.read_temperature()
+        pressure = BMP085sensor.read_pressure()
 
-	# Humidity and temp from DHT22 (outisde)
-	s = DHT22.sensor(pi, PIN_DHT22, LED=None, power=8)   
-	s.trigger()
-	time.sleep(0.2)
-	humidity=s.humidity()
-	temp1=s.temperature()
-	
-	si = DHT22.sensor(pi, PIN_DHT22i, LED=None, power=8)   
-	si.trigger()
-	time.sleep(0.2)
-	humidityi=si.humidity()
-	
-	# temp and pressure from BMP085
-	temp2=BMP085sensor.read_temperature()
-	pressure = BMP085sensor.read_pressure()
+        # calc average temp of 2 sensors
+        temperature = 0
+        temperature=(temp1+temp2)/2
 
-	# calc average temp of 2 sensors
-	temperature = 0
-	temperature=(temp1+temp2)/2
-	
-	# get reading from air quality sensor
-	#airq = 
-
-	update_thingspeak(temperature, humidity, pressure, humidityi)#, airq)
-	print "Temp (DHT22) " + str(temp1)
-	print "Temp (BMP)" + str(temp2)
-	print "Humidity (outide): " + str(humidity)
-	print "Humidity (inside): " + str(humidityi)
-	print "Pressure: " + str(pressure)	
-	
-	sys.exit()
+        update_thingspeak(temperature, humidity, pressure, airq)
+        print( "Temp (DHT22) " + str(temp1))
+        print( "Temp (BMP)" + str(temp2))
+        print( "Humidity (outide): " + str(humidity))
+        print( "Pressure: " + str(pressure))    
+        print( "Air Quality: " + str(aqi))    
+        
+        pi.stop() # Disconnect from Pi.
+        time.sleep(5)
