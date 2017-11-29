7942d171a4a2b9e39f6b4b46ca0a2e0627f33678
Matthew Macdonald-Wallace <matt@doics.co>
2017-11-29 13:35:41 +0000

    Add Dockerfile and sample script#

---
+++ sample.py
@@ -0,0 +1,23 @@
+import time
+import random
+
+import thingspeak
+import config
+
+ts = thingspeak.Channel(374732,"%s" % config.THINGSPEAK_APIWRITE)
+
+def random_values():
+    field_data = {}
+    field_data['api_key'] = config.THINGSPEAK_APIWRITE
+    field_data['channel_id'] = 374732
+    field_data['field1'] = random.randrange(-4,15)
+    field_data['field2'] = random.randrange(5,25)
+    field_data['field3'] = random.randrange(1,5)
+    field_data['field4'] = random.randrange(25,50)
+    return field_data
+
+while True:
+    print(random_values())
+    ts.update(random_values())
+    print(ts.get())
+    time.sleep(15)
