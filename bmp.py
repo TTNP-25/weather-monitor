d315d217541fadf1f2ce5f47968bb94cf47f2f0f
Matthew Macdonald-Wallace <matt@doics.co>
2017-11-30 09:55:52 +0000

    Fix issues in bmp module

--- bmp.py
+++ bmp.py
@@ -5,18 +5,18 @@
 # https://www.controleverything.com/content/Barometer?sku=BMP280_I2CSs#tabs-0-product_tabset-2
 
 
-import smbus
+import smself.bus
 import time
 
 class BMP():
 
-    def __init__():
-        # Get I2C bus
-        bus = smbus.SMBus(1)
+    def __init__(self):
+        # Get I2C self.bus
+        self.bus = smself.bus.SMBus(1)
 
         # BMP280 address, 0x76(118)
         # Read data back from 0x88(136), 24 bytes
-        self.b1 = bus.read_i2c_block_data(0x76, 0x88, 24)
+        self.b1 = self.bus.read_i2c_block_data(0x76, 0x88, 24)
 
 
     def get_data(self):
@@ -61,11 +61,11 @@ class BMP():
         # Select Control measurement register, 0xF4(244)
         #		0x27(39)	Pressure and Temperature Oversampling rate = 1
         #					Normal mode
-        bus.write_byte_data(0x76, 0xF4, 0x27)
+        self.bus.write_byte_data(0x76, 0xF4, 0x27)
         # BMP280 address, 0x76(118)
         # Select Configuration register, 0xF5(245)
         #		0xA0(00)	Stand_by time = 1000 ms
-        bus.write_byte_data(0x76, 0xF5, 0xA0)
+        self.bus.write_byte_data(0x76, 0xF5, 0xA0)
         
         time.sleep(0.5)
         
@@ -73,7 +73,7 @@ class BMP():
         # Read data back from 0xF7(247), 8 bytes
         # Pressure MSB, Pressure LSB, Pressure xLSB, Temperature MSB, Temperature LSB
         # Temperature xLSB, Humidity MSB, Humidity LSB
-        data = bus.read_i2c_block_data(0x76, 0xF7, 8)
+        data = self.bus.read_i2c_block_data(0x76, 0xF7, 8)
         
         # Convert pressure and temperature data to 19-bits
         adc_p = ((data[0] * 65536) + (data[1] * 256) + (data[2] & 0xF0)) / 16
